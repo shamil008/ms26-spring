eea430bb23a1064b26e83d868760139f100dd498
shamil008 <vasanovsamil07@gmail.com>
2024-07-05 14:03:41 +0400

    Commit api designs

--- src/main/java/com/example/ms/ingress/spring/controller/UserController.java
+++ src/main/java/com/example/ms/ingress/spring/controller/UserController.java
@@ -22,5 +22,5 @@ public class UserController {
 // v1/bookings/{id} -DELETE
 // v1/bookings/{id} -PUT
 // v1/bookings -POST
-// v1/lombard-credits/{id}?creditAmount=1000
+// v1/lombard-credits?fromAmount=1000&toAmount=5000&page=1&size=10&sortBy=date,DESC
 // v1/competitions/{competition_id}/teams/{team_id}
